5fd5ccc8885ed8a2492beae0c802859250348a18
Megan Chantosa <meganemmamoore@gmail.com>
2023-10-11 12:43:12 -0700

    finished main page

--- assets/js/main.js
+++ assets/js/main.js
@@ -197,6 +197,23 @@
   });
 
   /**
+   * About slider
+   */
+  new Swiper('.about-slider', {
+    speed: 400,
+    loop: true,
+    autoplay: {
+      delay: 2000,
+      disableOnInteraction: false
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    }
+  });
+
+  /**
    * Portfolio details slider
    */
   new Swiper('.portfolio-details-slider', {
